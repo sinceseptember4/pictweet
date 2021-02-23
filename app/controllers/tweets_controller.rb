c0c4c8720ca5ca10c82783fd1830fc4421a689c9
sinceseptember4 <sinceseptember4@gmail.com>
2021-02-23 16:11:18 +0000

    second

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,21 +1,42 @@
 class TweetsController < ApplicationController
-
-  before_action :move_to_index, except: :index
+ before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
   end
 
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
   end
 
   def move_to_index
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-      get   'tweets'      =>  'tweets#index' 
-  get   'tweets/new'  =>  'tweets#new'   
-  post  'tweets'      =>  'tweets#create'   
+  root  'tweets#index'
+  get   'tweets'      =>  'tweets#index'
+  get   'tweets/new'  =>  'tweets#new'
+  post  'tweets'      =>  'tweets#create'
+  delete  'tweets/:id'  => 'tweets#destroy'
+  get   'users/:id'  =>  'users#show'
 end
